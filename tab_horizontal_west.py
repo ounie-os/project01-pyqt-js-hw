414de9dc5d78211c5e3fcab68633c13a6c3e2b0c
ounie_os <ounie@qq.com>
2021-08-12 01:15:32 +0800

    修改tab bar显示样式。实现west方位的水平方向显示文字

--- tab_horizontal_west.py
+++ tab_horizontal_west.py
@@ -1,12 +1,13 @@
-import sys
-
-from PyQt5 import QtWidgets, QtCore
-from PyQt5.QtWidgets import QStyleFactory
-
+from PyQt5 import QtWidgets, QtCore, QtGui
 
 class TabBar(QtWidgets.QTabBar):
     def tabSizeHint(self, index):
         s = QtWidgets.QTabBar.tabSizeHint(self, index)
+
+        # 调整每个tab bar的尺寸与布局
+        s.setWidth(s.width() + 30)
+        s.setHeight(s.height() + 50)
+
         s.transpose()
         return s
 
@@ -14,18 +15,37 @@ class TabBar(QtWidgets.QTabBar):
         painter = QtWidgets.QStylePainter(self)
         opt = QtWidgets.QStyleOptionTab()
 
+        # 设置tab的字体
+        font = QtGui.QFont()
+        font.setFamily("黑体")
+        font.setPointSize(14)
+        painter.setFont(font)
+
+        # 设置tab的位置与尺寸
+        self.setGeometry(0, 180, 133, 240)
+
         for i in range(self.count()):
             self.initStyleOption(opt, i)
+
             painter.drawControl(QtWidgets.QStyle.CE_TabBarTabShape, opt)
             painter.save()
 
             s = opt.rect.size()
             s.transpose()
+
+            # 调整这里，使得文字能够完整显示
+            s.setWidth(s.width() + 75)
+            opt.rect.setSize(s)
+
             r = QtCore.QRect(QtCore.QPoint(), s)
             r.moveCenter(opt.rect.center())
-            opt.rect = r
 
             c = self.tabRect(i).center()
+
+            # 调整x，y坐标来调整文字在tab中的位置
+            c.setX(c.x() + 20)
+            c.setY(c.y() + 20)
+
             painter.translate(c)
             painter.rotate(90)
             painter.translate(-c)
@@ -36,18 +56,5 @@ class TabBar(QtWidgets.QTabBar):
 class HwTabWidget(QtWidgets.QTabWidget):
     def __init__(self, *args, **kwargs):
         QtWidgets.QTabWidget.__init__(self, *args, **kwargs)
-        self.setStyleSheet("background-color: rgb(85, 170, 255);alignment:center;")
         self.setTabBar(TabBar(self))
         self.setTabPosition(QtWidgets.QTabWidget.West)
-
-
-if __name__ == '__main__':
-    app = QtWidgets.QApplication(sys.argv)
-    app.setStyle(QStyleFactory.create("Fusion"))
-    w = HwTabWidget()
-    w.addTab(QtWidgets.QWidget(), "tab1")
-    w.addTab(QtWidgets.QWidget(), "tab2")
-    w.addTab(QtWidgets.QWidget(), "tab3")
-
-    w.show()
-    sys.exit(app.exec_())
